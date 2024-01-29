5a22aa3c99795713f22c679dca0e74d78109ae34
Antony Gakuru <antony123muriithi@gmail.com>
2024-01-29 22:30:30 +0300

    add login/signup toggle

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -106,8 +106,8 @@ function Signup() {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
               </svg>
             </div>
-            <h1 class="form__title text-[#007FFF]">Log in to your Account</h1>
-            <p class="form__description font-medium text-xl">Welcome back! Please, enter your information</p>
+            <h1 class="form__title text-[#007FFF]">Create an Account</h1>
+            <p class="form__description font-medium text-xl">We'd love to welcome you to our community!</p>
 
             <form>
               <label className="form-control__label">Email</label>
@@ -141,11 +141,11 @@ function Signup() {
                 onMouseOver={handleMouseOverSubmit}
                 onMouseOut={handleMouseOutSubmit}
               >
-                Log In
+                Sign up
               </button>
             </form>
             <p class="form__footer">
-              Don't have an account?<br /> <a href="#">Create an account</a>
+            Already have an account?<br /> <Link to="/login">Log In</Link>
             </p>
           </section>
 
